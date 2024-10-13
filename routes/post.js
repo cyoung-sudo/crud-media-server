29dc0c3db75ecfd42436e9881caaa42bf05a3cb9
Christopher Young <christopheryoung@MacBook123.local>
2024-10-12 17:38:04 -0700

    updated post routes

--- routes/post.js
+++ routes/post.js
@@ -32,4 +32,58 @@ postRoutes.route("/")
   .catch(err => console.log(err));
 });
 
+postRoutes.route("/:postId")
+//----- Update given post
+.get((req, res) => {
+  Post.findById(req.params.postId)
+  .then(doc => {
+    if(doc) {
+      res.json({
+        success: true,
+        post: doc
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "Post not found"
+      });
+    }
+  })
+  .catch(err => console.log(err));
+})
+//----- Update given post
+.put(checkAuthenticated, (req, res) => {
+  Post.findByIdAndUpdate(req.params.postId, {
+    title: req.body.title,
+    text: req.body.text
+  })
+  .then(doc => {
+    res.json({ success: true });
+  })
+  .catch(err => console.log(err));
+})
+//----- Delete given post
+.delete(checkAuthenticated, (req, res) => {
+  Post.findByIdAndDelete(req.params.postId)
+  .then(doc => {
+    res.json({ success: true });
+  })
+  .catch(err => console.log(err));
+})
+
+postRoutes.route("/user/:userId")
+//----- Retrieve posts for given user
+.get((req, res) => {
+  Post.find({
+    userId: req.params.userId
+  })
+  .then(docs => {
+    res.json({
+      success: true,
+      posts: docs
+    });
+  })
+  .catch(err => console.log(err));
+});
+
 export default postRoutes;
